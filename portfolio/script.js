73ac2fa08316a73ccd800f7259005d67a9acd455
Akhilmourya <113344668+Akhilmourya@users.noreply.github.com>
2023-03-29 09:00:22 -0700

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,9 @@
+// https://github.com/mattboldt/typed.js/ used of Typed function
+
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "Bussiness Developer", "Blogger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
